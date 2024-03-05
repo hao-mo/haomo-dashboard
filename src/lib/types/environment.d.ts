bd96c606ce30c368d010039f6cd039e5142f5e93
qqharry21 <qqharry21@gmail.com>
2024-03-05 16:29:07 +0800

    refactor: add some helpers & update metadata

--- src/lib/types/environment.d.ts
+++ src/lib/types/environment.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test' | 'local';
-    readonly NEXT_PUBLIC_BASE_URL: string;
+    readonly NEXT_PUBLIC_SITE_URL: string;
     readonly NEXT_PUBLIC_MEASUREMENT_ID: string;
     readonly NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: string;
     readonly SENTRY_DSN: string;
